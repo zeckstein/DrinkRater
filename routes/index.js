10cc3d9a45528c10e6df5c8c6aad50ce79922431
Zachary Eckstein <zacheckstein@live.com>
2018-10-11 14:22:06 -0400

    add auth middleware and navbar changes for login/logout

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var mid = require('../middleware');
 
 //GET /profile
-router.get('/profile', function(req,res,next){
-    if (! req.session.userId){
-        var err = new Error('You must log in to view this page!');
-        err.status = 403;
-        return next(err);
-    }
+router.get('/profile', mid.requiresLogin, function(req,res,next){
     User.findById(req.session.userId)
         .exec(function (error, user){
             if (error) {
@@ -20,7 +16,7 @@ router.get('/profile', function(req,res,next){
 });
 
 //GET /login
-router.get('/login', function(req,res,next){
+router.get('/login', mid.loggedOut, function(req,res,next){
     return res.render('login', { title: 'Log In'});
 });
 
@@ -58,7 +54,7 @@ router.get('/logout', function(req,res,next){
 
 
 //GET /register
-router.get('/register', function(req,res,next){
+router.get('/register', mid.loggedOut, function(req,res,next){
     return res.render('register', {title: 'Register'});
 });
 
